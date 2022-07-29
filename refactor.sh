b3494cb64ed5b6c4b69846242962e842a8935d71
Dhina17 <dhinalogu@gmail.com>
2022-07-29 16:56:11 +0530

    script: Add progress output

--- refactor.sh
+++ refactor.sh
@@ -18,17 +18,25 @@ read -rp "Enter app name (ex: Notes): " NEW_APP_NAME
 read -rp "Enter package name (ex: io.github.dhina17.notes): " NEW_PACKAGE
 
 # Create the package
+echo "Creating new package dir..."
 NEW_PACKAGE_DIR="$KOTLIN_SRC_DIR/$(echo "$NEW_PACKAGE" | sed 's/\./\//g')"
 mkdir -p "$NEW_PACKAGE_DIR"
 
 # Copy the template source files to new package
+echo "Copying the template source files..."
 cp -r $TEMPLATE_PACKAGE_DIR/. "$NEW_PACKAGE_DIR"
 
 # Remove the template package and files
+echo "Removing the template package dir..."
 rm -rf $TEMPLATE_PACKAGE_DIR
 
 # Change the package references in src
+echo "Updating the package references with new package..."
 find $APP_DIR -type f -not -path '*/\.*' -exec sed -i "s/$TEMPLATE_PACKAGE/$NEW_PACKAGE/g" {} \;
 
 # Change the app name references in all files
+echo "Updating the app name references with new name "
 find $ROOT_DIR -type f -not -path '*/\.*' -not -path '*.sh' -exec sed -i "s/$TEMPLATE_APP_NAME/$NEW_APP_NAME/g" {} \;
+
+# Final
+echo "Done. Please sync the project manually!"
