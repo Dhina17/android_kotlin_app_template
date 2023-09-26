5c51e6514a6c6aab551efbacf96c7e5983ade7b4
Dhina17 <dhinalogu@gmail.com>
2023-09-26 23:52:59 +0530

    gradle: Update safe-args to 2.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ buildscript {
     }
 
     dependencies {
-        val nav_version = "2.6.0"
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+        val navVersion = "2.7.3"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
