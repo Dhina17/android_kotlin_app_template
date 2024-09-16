0fe076315353b354cd91675ce70cc8a89c0cc355
Dhina17 <dhinalogu@gmail.com>
2024-09-16 21:27:12 +0530

    Bump dependencies
    Change-Id: I1f026513e864fa168c3c99a3cc0ca58c63a71402

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,27 +46,27 @@ android {
 dependencies {
 
     // Core
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
 
     // AppCompat
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
     // Material Design
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     // Navigation
-    val navigationVersion = "2.7.7"
+    val navigationVersion = "2.8.0"
     implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
 
 kapt {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.10" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
 }
 
@@ -19,7 +19,7 @@ buildscript {
     }
 
     dependencies {
-        val navVersion = "2.7.7"
+        val navVersion = "2.8.0"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
